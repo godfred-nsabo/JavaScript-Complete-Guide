d3360763754afd72b6cf8337b4652d39bb9c5e79
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-11-04 09:51:32 +0000

    Starting GAME APP

--- JavaScript/Functions - Rock Game/app.js
+++ JavaScript/Functions - Rock Game/app.js
@@ -1,32 +1,34 @@
-const startGameBtn = document.getElementById('start-game-btn')
+const startGameBtn = document.getElementById("start-game-btn");
 
-/*const start =  function startGame() { // using functions as expression
-  console.log('Game is about staring...');
-}
-/*function startGame() {
-  // using functions as declaration
-  console.log("Game is about staring...");
-}
-const start = function() {
-  // Anonymous function
-  console.log("Game is about staring...");
-}*/
-//startGame();
-/*const person = {
-  greet: function greet(){
-    console.log('Hello World') // A function in an object is called METHOD - A function attached to an object
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = PAPER;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function (param) {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ""
+  ).toUpperCase();
+  if (
+    selection !== ROCK && 
+    selection !== PAPER && 
+    selection !== SCISSORS
+    ) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
   }
+  return selection;
 };
-person.greet();*/
 
-/*console.log(typeof start);
-console.log(typeof start()); // gives Undefined
-console.dir(start);*/
-
-//startGameBtn.addEventListener('click', start); // AddeventListner is method on the object StartGameBtn 
-
-// Anonymous Function
-
-startGameBtn.addEventListener("click", function startGame() {
-  console.log("Game is about staring...", age);
+startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log("Game is about staring...");
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
