f22952897599651c8cdd3198812c0a1d4d70d718
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-11-04 07:49:36 +0000

    Functions

---
+++ JavaScript/Functions - Rock Game/app.js
@@ -0,0 +1,14 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Game is about staring...');
+}
+
+//startGame();
+const person = {
+  greet: function greet(){
+    console.log('Hello World') // A function in an object is called METHOD - A function attached to an object
+  }
+};
+person.greet();
+startGameBtn.addEventListener('click', startGame); // AddeventListner is method on the object StartGameBtn 
