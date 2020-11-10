0ae22e7214102c92fa4569a86e7d6a234cccd544
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-11-10 11:25:47 +0000

    changing DOM Properties

---
+++ JavaScript/Document and Window Object/app.js
@@ -0,0 +1,14 @@
+console.dir(document.getElementById('main-title'));
+console.dir(document.getElementsByClassName('.list-item'));
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+console.dir(h1.textContent);
+console.dir(h1.textContent = 'Dive into DOM');
+console.dir(h1.className = 'title');
+console.dir(h1.classList);
+console.dir(h1.style.color = 'white');
+console.dir(h1.style.backgroundColor = 'red');
+
+
+
