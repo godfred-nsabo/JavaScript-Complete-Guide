f465db7f83a8f976a50001011735cb7cd8cc2eaa
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-11-11 12:11:47 +0000

    Validating User Inputs

--- JavaScript/DOM PROJECT/assets/scripts/app.js
+++ JavaScript/DOM PROJECT/assets/scripts/app.js
@@ -12,10 +12,39 @@ console.log(addMovieModal1);
 const startMovieButton = document.querySelector('header button');
 console.log(startMovieButton);
 
-// other methods ofd selection;
-const startMovieButton = document.querySelector('header').lastElementChild;
-console.log(startMovieButton);
+const addBackdrop = document.getElementById('backdrop');
+// const addBackDrop = document.body.firstElementChild;
+const removeMovie = addMovieModal1.querySelector('.btn--passive');
+const addMovie = addMovieModal1.querySelector('.btn--success');
+const addMovie = removeMovie.nextElementSibling;
+// other methods of selection;
+//const startMovieButton = document.querySelector('header').lastElementChild;
+//console.log(startMovieButton);
 
+function toggleMovieModel () {
+    addMovieModal1.classList.toggle('visible');
+    togglebacdrop();
+};
 
+function togglebacdrop () {
+    addBackdrop.classList.toggle('visible')
+};
 
+function removeMovieButton () {
+    toggleMovieModel();
+};
+
+function addMovieButton() {
+    toggleMovieModel();
+};
+
+function backdropClickHandler () {
+    toggleMovieModel();
+};
+
+//EventListeners
+startMovieButton.addEventListener('click', toggleMovieModel);
+addBackdrop.addEventListener('click', toggleMovieModel);
+removeMovie.addEventListener('click', removeMovieButton);
+addMovie.addEventListener('click', addMovieButton);
 
