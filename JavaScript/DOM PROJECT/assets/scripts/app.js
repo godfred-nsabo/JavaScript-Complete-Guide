18ae7724dc0b8967295e05d23198f0f6cf77a369
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-11-11 13:32:29 +0000

    Fetching User Inputs

--- JavaScript/DOM PROJECT/assets/scripts/app.js
+++ JavaScript/DOM PROJECT/assets/scripts/app.js
@@ -6,45 +6,79 @@
 //const addMovieModal2 =  document.body.children[1];
 //console.log(addMovieModal2);
 
-const addMovieModal1 = document.querySelector('#add-modal');
+const addMovieModal1 = document.querySelector("#add-modal");
 console.log(addMovieModal1);
 
-const startMovieButton = document.querySelector('header button');
+const startMovieButton = document.querySelector("header button");
 console.log(startMovieButton);
 
-const addBackdrop = document.getElementById('backdrop');
+const addBackdrop = document.getElementById("backdrop");
 // const addBackDrop = document.body.firstElementChild;
-const removeMovie = addMovieModal1.querySelector('.btn--passive');
-const addMovie = addMovieModal1.querySelector('.btn--success');
-const addMovie = removeMovie.nextElementSibling;
+const removeMovie = addMovieModal1.querySelector(".btn--passive");
+const addMovie = addMovieModal1.querySelector(".btn--success");
+//const addMovie = removeMovie.nextElementSibling; //alternative approach
+
 // other methods of selection;
 //const startMovieButton = document.querySelector('header').lastElementChild;
 //console.log(startMovieButton);
+const userInputs = addMovieModal1.querySelectorAll("input");
+//const userInputs = addMovieModal1.getElementsByTagName('input');
 
-function toggleMovieModel () {
-    addMovieModal1.classList.toggle('visible');
-    togglebacdrop();
-};
+//Functions
+function toggleMovieModel() {
+  addMovieModal1.classList.toggle("visible");
+  togglebackdropHandler();
+}
 
-function togglebacdrop () {
-    addBackdrop.classList.toggle('visible')
-};
+function togglebackdropHandler() {
+  addBackdrop.classList.toggle("visible");
+}
 
-function removeMovieButton () {
-    toggleMovieModel();
-};
+function removeMovieButtonHandler() {
+  toggleMovieModel();
+}
 
-function addMovieButton() {
-    toggleMovieModel();
-};
+function addMovieButtonHandler() {
+  //toggleMovieModel();
+  const titleValue = userInputs[0].value;
+  const imageValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
 
-function backdropClickHandler () {
-    toggleMovieModel();
-};
+  if (titleValue.trim() === "") {
+    alert("Please enter valid Inputs");
+  } else if (imageValue.trim() === "") {
+    alert("Please enter valid Inputs");
+  } else if (ratingValue.trim() === "") {
+    alert("Please enter valid Inputs");
+  } else if (+ratingValue < 1 || +ratingValue > 5) {
+    alert("Please enter valid inputs!");
+  }
+}
+
+function addMovieButtonHandler() {
+  //toggleMovieModel();
+  const titleValue = userInputs[0].value;
+  const imageValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+   imageValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 || 
+    +ratingValue > 5
+){
+    alert("Please enter valid inputs!");
+    return;
+}
+
+
+function backdropClickHandler() {
+  toggleMovieModel();
+}
 
 //EventListeners
-startMovieButton.addEventListener('click', toggleMovieModel);
-addBackdrop.addEventListener('click', toggleMovieModel);
-removeMovie.addEventListener('click', removeMovieButton);
-addMovie.addEventListener('click', addMovieButton);
-
+startMovieButton.addEventListener("click", toggleMovieModel);
+addBackdrop.addEventListener("click", toggleMovieModel);
+removeMovie.addEventListener("click", removeMovieButtonHandler);
+addMovie.addEventListener("click", addMovieButtonHandler);
